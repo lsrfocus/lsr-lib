de3a15b3379e9d09eb55d790785dddf3b4633c14
Kevin Cooper <cooperka92@gmail.com>
2018-11-01 21:17:54 -0400

    :package: Also convert generateIndex to typescript

--- packages/lsr-lib/src/scripts/generateIndex.ts
+++ packages/lsr-lib/src/scripts/generateIndex.ts
@@ -5,27 +5,33 @@ import writeFile from 'write';
 import * as _ from 'lodash';
 import Immutable from 'immutable';
 
+type PackageNameMap = Immutable.Map<string, string | null>;
+type PackageExports = Immutable.Set<string>;
+type PackageExportsMap = Immutable.Map<string, PackageExports>;
+
 const glob = util.promisify(sadGlob);
 
 (async () => {
   const allPackageNames = await glob('../*');
 
   // Map from package name to Set of all named exports.
-  const packagesToExport = Immutable.Set(allPackageNames)
-    .map((dirPath) => path.basename(dirPath))
+  const packagesToExport: PackageExportsMap = Immutable.Set(allPackageNames)
+    .map((dirPath) => path.basename(dirPath || ''))
+    // @ts-ignore
     .delete('lsr-lib')
     .reduce(
-      (reduction, packageName) => reduction.set(packageName, null),
+      (reduction: PackageNameMap, packageName: string) => reduction.set(packageName, null),
       Immutable.Map()
     )
-    .map((dummy, packageName) => {
+    .map((dummy: PackageNameMap, packageName: string) => {
       const lib = require(packageName);
       const exports = _.keys(lib);
       return Immutable.Set(exports).delete('default');
     });
 
   const exportContent = packagesToExport
-    .map((exports, packageName) => `export { ${exports.join(', ')} } from '${packageName}';`)
+    .map((exports: PackageExports = Immutable.Set(), packageName: string = '') =>
+      `export { ${exports.join(', ')} } from '${packageName}';`)
     .join('\n');
 
   const content = `// AUTO-GENERATED, DO NOT EDIT.\n\n${exportContent}\n`;
